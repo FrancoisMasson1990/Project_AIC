5a8372c637c6a2bd609698fa8d6a8067f24bd9a9
francoismasson1990 <francois-masson@hotmail.com>
2021-07-17 08:54:34 -0400

    cropping images

--- AI_2D/plot_inference_results.py
+++ AI_2D/plot_inference_results.py
@@ -24,9 +24,9 @@ from model.py, trains the model on the data, and then saves the
 best model.
 """
 
-import multiprocessing
+import numpy as np
+from aic_models import data_preprocess as dp
 import psutil
-import datetime
 import os
 import tensorflow as tf
 from tensorflow import keras as K
@@ -37,6 +37,7 @@ import psutil
 import yaml
 import matplotlib.pyplot as plt
 from pathlib import Path
+from tqdm import tqdm
 from aic_models.model_2D import unet
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # Get rid of the AVX, SSE warnings
@@ -97,6 +98,8 @@ def soft_dice_coef(target, prediction, axis=(1, 2), smooth=0.0001):
     where T is ground truth mask and P is the prediction mask
     """
 
+    prediction = K.backend.round(prediction)  # Round to 0 or 1
+    
     intersection = tf.reduce_sum(target * prediction, axis=axis)
     union = tf.reduce_sum(target + prediction, axis=axis)
     numerator = tf.constant(2.) * intersection + smooth
@@ -105,10 +108,10 @@ def soft_dice_coef(target, prediction, axis=(1, 2), smooth=0.0001):
 
     return tf.reduce_mean(coef)
 
-def tversky(self, target, prediction, smooth=1, alpha=0.7):
+def tversky(self, target, prediction, smooth=1, alpha=0.7,channels_first=False):
 
     # Flatten the input data
-    if self.channels_first:
+    if channels_first:
         y_true = K.backend.permute_dimensions(target, (3,1,2,0))
         y_pred = K.backend.permute_dimensions(prediction, (3,1,2,0))
     else :
@@ -123,41 +126,86 @@ def tversky(self, target, prediction, smooth=1, alpha=0.7):
     return (true_pos + smooth) / (true_pos + alpha * false_neg +
                                 (1 - alpha) * false_pos + smooth)
 
-def plot_results(imgs,labels,model,folder,number):
+def plot_results(imgs,labels,model,folder,name,model_version):
     """
     Calculate the Dice and plot the predicted masks for image # img_no
     """
 
-    # Prediction
-    if model is not None:
-        predictions = model.predict(imgs)
-    
+    # Image processing
+    imgs = dp.load_scan(imgs)
+    imgs = dp.get_pixels_hu(imgs)
+
+    if crop_dim != -1:
+        imgs = dp.crop_dim(imgs,crop_dim=crop_dim)
+
+    min_ = int(z_slice_min*imgs.shape[0])
+    max_ = int(z_slice_max*imgs.shape[0])
+    index_z_crop = np.arange(min_,max_)
+    imgs = imgs[index_z_crop]
+
+    if model_version == 0:  # old version, input images were normalized for each slice
+        imgs = dp.preprocess_inputs(img)
+    elif model_version == 1: # new version, input images were normalized according to z
+        imgs = dp.preprocess_img(imgs)
+        imgs = np.expand_dims(imgs,-1)
+
+    # label processing
+    labels = dp.preprocess_label(labels)
+    if crop_dim != -1:
+        labels = dp.crop_dim(labels,crop_dim=crop_dim)
+
+    labels = labels[index_z_crop]
+
+    # Init Figure
+    if model is None:
+        fig, ax = plt.subplots(1, 2, figsize=(15, 15))
+        ax0, ax1 = ax[0], ax[1]
+    else :
+        fig, ax = plt.subplots(1, 3, figsize=(15, 15))
+        ax0, ax1, ax2 = ax[0], ax[1], ax[2]
+
+    ax0_object = None
+    ax1_object = None
+    ax2_object = None
     for i in range(imgs.shape[0]):
-        # Init Figure
-        plt.figure(figsize=(20,20))
+        
+        # Prediction
+        if model is not None:
+           prediction = np.expand_dims(imgs[i,:,:,:], 0)
+           prediction = model.predict(prediction)
 
         # Image 
-        plt.subplot(1,3,1)
-        plt.imshow(imgs[i, :, :, 0], cmap="bone", origin="lower")
-        plt.title("MRI")
-        plt.axis("off")
+        if ax0_object is None:
+            ax0_object = ax0.imshow(imgs[i, :, :, 0], cmap="bone", origin="lower")
+        else :
+            ax0_object.set_data(imgs[i, :, :, 0])
+
+        ax0.set_title("MRI")
+        ax0.axis("off")
 
         # Label
-        plt.subplot(1, 3, 2)
-        plt.imshow(labels[i, :, :, 0],origin="lower",vmin=0, vmax=1)
-        plt.title("Ground Truth")
-        plt.axis("off")
+        if ax1_object is None:
+            ax1_object = ax1.imshow(labels[i, :, :],origin="lower",vmin=0, vmax=1)
+        else :
+            ax1_object.set_data(labels[i, :, :])
+
+        ax1.set_title("Ground Truth")
+        ax1.axis("off")
 
         if model is not None:
-            plt.subplot(1, 3, 3)
-            plt.imshow(predictions[i, :, :, 0],origin="lower",vmin=0, vmax=1)
-            plt.title("Predictions\n(Dice {:.4f}, Soft Dice {:.4f})".\
-                      format(dice_coef(labels[i],predictions[i]),soft_dice_coef(labels[i],predictions[i])))
-            #plt.title("Predictions\n(Tversky {:.4f})".\
-            #           format(dice_coef(labels[i],predictions[i])))
-            plt.axis("off")
+            if ax2_object is None:
+                ax2_object = ax2.imshow(prediction[0, :, :, 0],origin="lower",vmin=0, vmax=1)
+            else :
+                ax2_object.set_data(prediction[0, :, :, 0])
+            
+            ax2.set_title("Predictions\n(Dice {:.4f}, Soft Dice {:.4f})".\
+                            format(dice_coef(np.expand_dims(labels[i,:,:],(0,-1)),prediction),
+                            soft_dice_coef(np.expand_dims(labels[i,:,:],(0,-1)),prediction)))
+            #ax2.set_title("Predictions\n(Tversky {:.4f})".\
+            #          format(dice_coef(np.expand_dims(labels[i,:,:],(0,-1)),prediction)))
+            ax2.axis("off")
         
-        png_filename = os.path.join(folder, "pred_{}.png".format(number+i))
+        png_filename = os.path.join(folder, "pred_{}_{}.png".format(name,i))
         plt.savefig(png_filename, bbox_inches="tight", pad_inches=0)
 
 
@@ -174,7 +222,7 @@ if __name__ == "__main__":
 
     data_path = config.get("data_path",None)
     batch_size = config.get("batch_size",None)
-    crop_dim = config.get("crop_dim",None)
+    crop_dim = config.get("crop_dim",-1)
     channels_first = config.get("channels_first",None)
     featuremaps = config.get("featuremaps",None)
     output_path = config.get("output_path",None)
@@ -182,8 +230,8 @@ if __name__ == "__main__":
     z_slice_min = config.get("z_slice_min",None)
     z_slice_max = config.get("z_slice_max",None)
 
-    #model_filename = "/home/francoismasson/Project_AIC/Viewer/models/unet_model_for_aic_512.hdf5"
-    model_filename = "/home/francoismasson/Project_AIC/output/unet_model_for_aic_test.hdf5"
+    model_version = 1
+    model_filename = "/home/francoismasson/Project_AIC/Viewer/models/unet_model_for_aic_320.hdf5"
 
     """
     Load a model, load the data, and see inference.
@@ -197,23 +245,16 @@ if __name__ == "__main__":
     print("-" * 30)
 
     trainFiles,trainLabels,validateFiles,validateLabels,testFiles,testLabels = get_filelist(data_path=data_path)
-    
-    # This is the maximum value one of the files haves. 
-    # Required because model built with assumption all file same z slice.
-    num_slices_per_scan = slice_filelist(data_path=data_path)
-    ds_train = DatasetGenerator(trainFiles,trainLabels,num_slices_per_scan,batch_size=batch_size,\
-                                crop_dim=[crop_dim,crop_dim], augment=True,imbalanced=True, z_slice_min=-1, z_slice_max=-1)
-    ds_validation = DatasetGenerator(validateFiles,validateLabels,num_slices_per_scan,batch_size=batch_size,\
-                                crop_dim=[crop_dim,crop_dim], augment=False, z_slice_min=z_slice_min, z_slice_max=z_slice_max)
-    ds_test = DatasetGenerator(testFiles,testLabels,num_slices_per_scan,batch_size=batch_size,\
-                                crop_dim=[crop_dim,crop_dim], augment=False, z_slice_min=z_slice_min, z_slice_max=z_slice_max)
      
     unet_model = unet()
-    try :
-        model = unet_model.load_model(model_filename)
-    except :
+    #model_filename = None
+    if model_filename is not None:
+        if model_version == 0:
+            model = unet_model.load_model(model_filename,False)
+        elif model_version == 1:
+            model = unet_model.load_model(model_filename)
+    else :
         model = None
-    #model = None
 
     # Create output directory for images
     png_directory = "inference_examples"
@@ -223,14 +264,10 @@ if __name__ == "__main__":
 
     png_folder = os.path.join(Path.cwd(),png_directory)
 
-    # The plots will be saved to the png_directory (Keep only the first batch for now)
-    number = 0
-    loader = ds_test
-    
-    for count,(img,label) in enumerate(loader.ds):
-        if number < 400 :
-            # Will show only 100 first prediction
-            plot_results(img,label,model,png_folder,number)
-            number += img.shape[0]
-        else :
-            break
+    # The plots will be saved to the png_directory
+    imgs = trainFiles
+    labels = trainLabels
+
+    for index,(img,label) in tqdm(enumerate(zip(imgs,labels)),total=len(imgs)): 
+        valve_name = str(Path(img).parent).split('/')[-1]
+        plot_results(img,label,model,png_folder,valve_name,model_version)
