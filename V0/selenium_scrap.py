948b6d10c8cf21fc081e107f64a55b7ba614eb98
Francois Masson <francois.masson@contxtful.com>
2022-01-21 16:12:55 -0500

    dynamic selenium

--- V0/selenium_scrap.py
+++ V0/selenium_scrap.py
@@ -1,8 +1,9 @@
-from grpc import services
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+import pandas as pd
+from tqdm import tqdm
 
 path = "../chromedriver/chromedriver"
 browserpath = "/opt/google/chrome/google-chrome"
@@ -15,13 +16,43 @@ options.add_argument('window-size=1200x900')
 service = Service(path)
 driver = webdriver.Chrome(service=service, options=options)
 url = "https://dappradar.com/nft/collections"
-class_name = "cms-page" #CLASS_NAME
-#url = "https://nftgo.io/whale-tracking/whale"
-#class_name = "whales_container__mmGF3"
+url = "https://cryptoslam.io/nfts"
+
+time.sleep(1)
 driver.get(url=url)
 
+columns = ["Collection","Website","Twitter","Discord"]
+df = pd.DataFrame(columns=columns)
+x_path = '//*[@id="table"]/tbody'
 
-print("Element identified by class:",driver.find_element(By.ID, "rankCollectionTable").text)
+elems = driver.find_elements(By.XPATH, x_path)
+while not elems:
+    driver.close()
+    driver.quit()
 
-driver.close()
-exit()
+    time.sleep(1)
+    service = Service(path)
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get(url=url)
+    elems = driver.find_elements(By.XPATH, x_path)
+
+    print("Failed. Retry starting service")
+
+for elem in elems:
+    rows = elem.find_elements(By.XPATH,'//*[@class="odd"]')
+    for row in tqdm(rows):
+        if row.text:
+            # To search relative to a particular element, 
+            # you should prepend the expression with . instea
+
+            hrefs = row.find_elements(By.XPATH, './/*[@href]')
+            for href in hrefs:
+                if "twitter" in href.get_attribute("href"):
+                    print(href.get_attribute("href"))
+                elif "discord" in href.get_attribute("href"):
+                    print(href.get_attribute("href"))
+            
+            collection_name = row.text.split(" ")[3]
+            print(collection_name)
+        # Need to add webiste
+        #df.loc[len(df)] = ""
