ab4b030827859c266ae73ad2d93d79d3243d89f1
francoismasson1990 <francois-masson@hotmail.com>
2022-10-02 13:53:49 -0400

    change route name

--- scripts/web/app.py
+++ scripts/web/app.py
@@ -5,19 +5,19 @@ from flask import render_template
 
 from aic.dashboard.dashapp import create_dash_app
 
-server, model, online = AppContext.app()
-app = create_dash_app(server, "/dash/", model, online)
+flask_app, model, online = AppContext.app()
+dash_app = create_dash_app(flask_app, "/dash/", model, online)
 
 
-@server.route("/")
-def dash_app() -> Any:
+@flask_app.route("/")
+def home_index() -> Any:
     return render_template("index.html")
 
 
-@server.route("/dash")
-def index():
-    return app.index()
+@flask_app.route("/dash")
+def dash_index():
+    return dash_app.index()
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port="8000", debug=True)
+    flask_app.run(host="0.0.0.0", port="8000", debug=True)
